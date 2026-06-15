5e31f84e0ccd2970321033cdfbda6c555be86313
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-06-15 09:52:57 -0500

    Rovo Mcp config

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -1,41 +1,14 @@
-devsession='dev'
-autosession='auto'
-rxlocalsession='rxlocal'
+#!/bin/sh
 
 #--- Dev Session ---
-tmux new-session -d -s $devsession
+tmux new-session -d -s "dev" -n "nvim" -c "~/.config/nvim"
+tmux new-window "dev:1" -n "ouro" -c "/Users/rickbordelon/Documents/Projects/Ouro"
+tmux new-window "dev:2" -n "dotfiles" -c "/Users/rickbordelon/Documents/Projects/dotfiles"
 
-tmux rename-window -t 0 'api'
-tmux send-keys -t 'api' 'zsh' C-m
-tmux send-keys -t 'api' 'cd /Users/rickbordelon/Documents/Projects/redsail-mobile-mono/legacy/PioneerRx.Mobile.Api' C-m
-tmux send-keys -t 'api' 'clear' C-m
-
-tmux new-window -t $devsession:1 -n 'mobile'
-tmux send-keys -t 'mobile' 'zsh' C-m
-tmux send-keys -t 'mobile' 'cd /Users/rickbordelon/Documents/Projects/redsail-mobile-mono' C-m
-tmux send-keys -t 'mobile' 'clear' C-m
-
-tmux new-window -t $devsession:2 -n 'ouro'
-tmux send-keys -t 'ouro' 'zsh' C-m
-tmux send-keys -t 'ouro' 'cd /Users/rickbordelon/Documents/Projects/Ouro' C-m
-tmux send-keys -t 'ouro' 'clear' C-m
-
-tmux new-window -t $devsession:3 -n 'nvim'
-tmux send-keys -t 'nvim' 'zsh' C-m
-tmux send-keys -t 'nvim' 'cd ~/.config/nvim/' C-m
-tmux send-keys -t 'nvim' 'clear' C-m
+#--- Mobile Session ---
+tmux new-session -d -s "mobile" -n "api" -c "/Users/rickbordelon/Documents/Projects/redsail-mobile-mono/legacy/PioneerRx.Mobile.Api"
+tmux new-window "mobile:1" -n "mobile" -c "/Users/rickbordelon/Documents/Projects/redsail-mobile-mono"
 
 #--- RxLocal Session ---
-tmux new-session -d -s $rxlocalsession
-tmux rename-window -t 0 'work'
-tmux send-keys -t 'work' 'zsh' C-m
-tmux send-keys -t 'work' 'cd /Users/rickbordelon/Documents/Projects/rxlocal-workspace' C-m
-
-#--- Automation Session ---
-tmux new-session -d -s $autosession
-tmux rename-window -t 0 'auto'
-tmux send-keys -t 'auto' 'zsh' C-m
-tmux send-keys -t 'auto' 'cd ' C-m
-tmux send-keys -t 'auto' 'cd /Users/rickbordelon/Documents/Projects/PioneerRxAutomation' C-m
-tmux send-keys -t 'auto' 'source ~/py_envs/bin/activate' C-m
-tmux send-keys -t 'auto' 'clear' C-m
+tmux new-session "rxlocal" -n "work" -c "/Users/rickbordelon/Documents/Projects/rxlocal-workspace"
+tmux new-window "rxlocal:1" -n "ouro" -c "/Users/rickbordelon/Documents/Projects/Ouro"
