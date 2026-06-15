ca9f020612bcc290b84ac0189a1f968fe16a302f
Rick Bordelon <rick.bordelon@redsailtechnologies.com>
2026-06-15 09:53:32 -0500

    Fixes to tmux_startup

--- tmux_startup.sh
+++ tmux_startup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 #--- Dev Session ---
-tmux new-session -d -s "dev" -n "nvim" -c "~/.config/nvim"
-tmux new-window "dev:1" -n "ouro" -c "/Users/rickbordelon/Documents/Projects/Ouro"
-tmux new-window "dev:2" -n "dotfiles" -c "/Users/rickbordelon/Documents/Projects/dotfiles"
+tmux new-session -d -s "dev" -n "nvim" -c "/Users/rickbordelon/.config/nvim"
+tmux new-window -t "dev:1" -n "ouro" -c "/Users/rickbordelon/Documents/Projects/Ouro"
+tmux new-window -t "dev:2" -n "dotfiles" -c "/Users/rickbordelon/Documents/Projects/dotfiles"
 
 #--- Mobile Session ---
 tmux new-session -d -s "mobile" -n "api" -c "/Users/rickbordelon/Documents/Projects/redsail-mobile-mono/legacy/PioneerRx.Mobile.Api"
-tmux new-window "mobile:1" -n "mobile" -c "/Users/rickbordelon/Documents/Projects/redsail-mobile-mono"
+tmux new-window -t "mobile:1" -n "mobile" -c "/Users/rickbordelon/Documents/Projects/redsail-mobile-mono"
 
 #--- RxLocal Session ---
-tmux new-session "rxlocal" -n "work" -c "/Users/rickbordelon/Documents/Projects/rxlocal-workspace"
-tmux new-window "rxlocal:1" -n "ouro" -c "/Users/rickbordelon/Documents/Projects/Ouro"
+tmux new-session -d -s "rxlocal" -n "work" -c "/Users/rickbordelon/Documents/Projects/rxlocal-workspace"
